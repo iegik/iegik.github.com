b0bf2fe3c0c18d5c26c8200e189fdff429525691
Янсон Артур <a.iansons@petrovich.ru>
2016-10-23 02:12:14 +0300

    Fix animation performance

--- src/components/core/index.js
+++ src/components/core/index.js
@@ -2,6 +2,8 @@
     location.hash = location.hash || "#home";
     var x1, x2, x, y,
         k = 16,
+        requestID,
+        backgroundPosition,
         body = document.body,
         html = document.documentElement,
         width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth),
@@ -11,17 +13,28 @@
     width > 300 && addEventListener("mousemove", function (a) {
         x = a.pageX, y = a.pageY
     });
-    function animate() {
+
+    function calculate(){
         x1 -= .5;
         x2 -= 5;
-        body.style.backgroundPosition = width / 2 - 2762 - x / (5 * k) + "px " + (
-                height - 129 + 20 - y / (10 * k)
+        backgroundPosition = width / 2 - 2762 - x / (5 * k) + "px " + (
+                height - 129 + (20 - y) / (10 * k)
             ) + "px, " + (
-                x1 - x / (10 * k)
+                (x1 - x) / (10 * k)
             ) + "px 60%, " + (
-                x2 - x / k
+                (x2 - x) / k
             ) + "px 40%";
-        requestAnimationFrame(animate);
+    }
+    setInterval(function(){
+        calculate();
+    }, 25);
+
+    function animate() {
+        body.style.backgroundPosition = backgroundPosition;
+        if(requestID){
+            cancelAnimationFrame(requestID);
+        }
+        requestID = requestAnimationFrame(animate);
     }
     animate();
 }(document));
