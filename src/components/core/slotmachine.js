ae531b77345b399a19d039bbdf644cdea18493c6
Arturs Jansons <a.jansons@gmail.com>
2020-02-23 23:05:10 +0300

    Make alert appear after render

--- src/components/core/slotmachine.js
+++ src/components/core/slotmachine.js
@@ -1,14 +1,20 @@
 ;(slotmachine => {
     let tries = 0;
+    let blocked = false;
     const roll = '🍏,🌰,🍋,🍅,🍆,🍇,🍓,🍉,🍐,🍒,🍑,🥑'.split(',');
     const { floor, random } = Math;
     const randomRoll = what => what[floor(random() * what.length)];
     const play = _ => {
+      if (blocked) return;
       const [a, b, c] = [roll, roll, roll].flatMap(randomRoll);
       slotmachine.innerHTML = `${a} ${b} ${c}`;
       if (a == b && b == c) {
+          blocked = true;
           let msg = `You won in ${tries} tries`;
-          setTimeout(() => alert(msg));
+          setTimeout(() => {
+            alert(msg);
+            blocked = false;
+          }, 100);
           tries = 0;
       } else {
           tries++;
