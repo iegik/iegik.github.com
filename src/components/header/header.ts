81fd5f5850f82df2fd16acaf90998c101a06f49a
Arturs Jansons <a.jansons@gmail.com>
2022-01-30 02:20:00 +0300

    Update responsive images

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -3,7 +3,7 @@ import ascii from '@assets/home/images/artursjansons.ascii'
 
 const Header = () => `
     <header class="header">
-      <a href="#home" class="header__avatar" alt="avatar">${Image({ itemprop: "photo", src: `home/images/artursjansons.jpg`, ratio: "1:1", alt: ascii })}</a>
+      <a href="#home" class="header__avatar" alt="avatar">${Image({ itemprop: "photo", src: `home/images/artursjansons.jpg`, ratio: "1:1", sizes: "255,216,64", alt: ascii })}</a>
     </header>
   `
 export default Header
--- src/components/image/image.ts
+++ src/components/image/image.ts
@@ -2,16 +2,21 @@ const mask = /.(png|jpg)$/
 const jpg = /.jpg$/
 const png = /.png$/
 
+const types = {
+  'image/webp': 'webp',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+}
+
 export default (props) => {
-  const { src, ratio, loading, alt = '', itemprop  } = props;
+  const { src, ratio, sizes, loading, alt = '', itemprop  } = props;
   const [width, height] = ratio.split(':')
 
   if (jpg.test(src)) {
     return `
       <picture>
-        <source type="image/webp" srcset="${src.replace(mask,'.webp')}" />
-        <source type="image/jpeg" srcset="${src.replace(mask,'.jpg')}" />
-        <img src="${src.replace(mask,'.png')}" width="${width}" height="${height}" loading="${loading}" alt="${alt}" itemprop="${itemprop}" />
+        ${Object.entries(types).map(([type, ext]) => `<source type="${type}" srcset="${sizes.split(',').map((w) => src.replace(mask,`_${w}.${ext} ${w}w`))}" />`).join('')}
+        <img src="${src}" width="${width}" height="${height}" loading="${loading}" alt="${alt}" itemprop="${itemprop}" />
       </picture>
     `
   }
