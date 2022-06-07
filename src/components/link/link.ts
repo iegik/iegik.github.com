bbdefb0fcd2e6282eb4e7773c9328e7fbc5a5f13
Arturs Jansons <a.jansons@gmail.com>
2022-06-07 20:24:28 +0300

    Update info

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -1,4 +1,4 @@
-interface Props {
+interface LinkProps {
   className?: string;
   children?: string | string[]
   tag?: string
@@ -6,8 +6,8 @@ interface Props {
   title?: string
 }
 
-const Link:FC<Props> = (props) => {
-  const { className = '', children, tag = 'a', to = '#', title = ''} = props
+const Link:FC<LinkProps> = (props) => {
+  const { className = 'link', children, tag = 'a', to = '#', title = ''} = props
 
   return `
       <${tag} class="${className} link" href="${to}" title="${title}">
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,21 +13,21 @@ const Home = () => `
       <section id="home" itemscope itemtype="https://schema.org/Person">
         <header>
           <h2 itemprop="name" class="h2"><span itemprop="givenName">Artūrs</span> <span itemprop="familyName">Jansons</span></h2>
-          <span itemprop="jobTitle" class="subtitle">Full-stack web developer</span>
+          <span itemprop="jobTitle" class="subtitle">Software Developer</span>
             <br>
             <span itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
-              <span itemprop="addressLocality">Europe</span>
+              <span itemprop="addressLocality">🇹🇷 Alanya, Turkey</span>
             </span>
-            ${/* <a href="callto:0079213900737"><i class="fa fa-whatsapp"></i><span itemprop="telephone">+7-921-390-07-37</span></a> */ ''}
+            <a href="callto:+905340658509"><i class="fa fa-whatsapp"></i><span itemprop="telephone">+9-053-406-58-509</span></a>
             <a href="mailto:a.jansons+github@gmail.com?title=iegik.github.io:contact" itemprop="email">a.jansons@gmail.com</a>
           ${LinkList({
     squire: true,
     children: [
-      Link({ className: 'link-list__link', to: 'https://linkedin.com/in/iegik', title: 'LinkedIn', children: [Icon({ name: "linkedin-icon", className: 'link-list__icon' })]}),
-      Link({ className: 'link-list__link', to: 'https://github.com/iegik', title: 'GitHub', children: [Icon({ name: "github-icon", className: 'link-list__icon' })]}),
-      Link({ className: 'link-list__link', to: 'https://profile.codersrank.io/user/iegik', title: 'Coders Rank', children: [Icon({ name: "codersrank-icon", className: 'link-list__icon' })]}),
-      Link({ className: 'link-list__link', to: 'https://jsfiddle.net/user/iegik', title: 'JSFiddle', children: [Icon({ name: "jsfiddle-icon", className: 'link-list__icon' })]}),
-      Link({ className: 'link-list__link', to: 'https://stackoverflow.com/users/771471/iegik', title: 'Stack Overflow', children: [Icon({ name: "stackoverflow-icon", className: 'link-list__icon' })]}),
+      Link({ to: 'https://linkedin.com/in/iegik', title: 'LinkedIn', children: [Icon({ name: "linkedin-icon", className: 'link-list__icon' })]}),
+      Link({ to: 'https://github.com/iegik', title: 'GitHub', children: [Icon({ name: "github-icon", className: 'link-list__icon' })]}),
+      Link({ to: 'https://profile.codersrank.io/user/iegik', title: 'Coders Rank', children: [Icon({ name: "codersrank-icon", className: 'link-list__icon' })]}),
+      Link({ to: 'https://jsfiddle.net/user/iegik', title: 'JSFiddle', children: [Icon({ name: "jsfiddle-icon", className: 'link-list__icon' })]}),
+      Link({ to: 'https://stackoverflow.com/users/771471/iegik', title: 'Stack Overflow', children: [Icon({ name: "stackoverflow-icon", className: 'link-list__icon' })]}),
     ],
   })}
           <div class="network">
