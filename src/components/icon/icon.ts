5d6a427b2afb99c2bbf24108466b3992905b965d
Arturs Jansons <a.jansons@gmail.com>
2022-06-15 20:46:19 +0300

    Revert icon dynamic viewBox change

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -1,10 +1,21 @@
 import { createRef } from '@app/components/core/view';
 import { spriteMap } from '@app/components/Sprite';
 
-const Icon = ({ className, name }) => `
-  <svg class="${className}" preserveAspectRatio="xMidYMid meet" viewBox="${spriteMap.get(name)?.viewBox || '0 0 0 0'}">
-    <use xlink:href="#sprite-${name}" />
-  </svg>
-`
+const Icon = ({ className, name }) => {
+  const ref = createRef();
+
+  setTimeout(() => {
+    if (typeof window === 'undefined') return;
+
+    const viewBox = document.getElementById(`sprite-${name}`).getAttribute('viewBox')
+    ref.current.setAttribute('viewBox', viewBox)
+  })
+
+  return `
+    <svg class="${className}" preserveAspectRatio="xMidYMid meet" viewBox="${spriteMap.get(name)?.viewBox || '0 0 0 0'}" ref="${ref}">
+      <use xlink:href="#sprite-${name}" />
+    </svg>
+  `
+}
 
 export default Icon
