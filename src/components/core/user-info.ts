a37ae91b4d088ec3d673a81d270ae54c86a5a907
Arturs Jansons <a.jansons@gmail.com>
2022-06-15 16:16:54 +0300

    Fix recursion on user-info component

--- src/components/core/user-info.ts
+++ src/components/core/user-info.ts
@@ -3,14 +3,17 @@ import { createRef } from '@app/components/core/view';
 const UserInfo:FC = ({ avatar_url, login } = {}) => {
   const ref = createRef();
 
-  setTimeout(async () => {
-    // Getting state
-    const access_token = sessionStorage.getItem('access_token')
-    const token_type = sessionStorage.getItem('token_type')
-    console.debug('Requesting user', { access_token, token_type })
-    const data = await fetch('https://api.github.com/user', { method: 'GET', headers: { 'Authorization': `${token_type} ${access_token}` } }).then((res) => res.json())
-    console.debug('User data', { data })
-    ref.current.innerHTML = UserInfo(data)
+  setTimeout(() => {
+    const fetchUser = async () => {
+      // Getting state
+      const access_token = sessionStorage.getItem('access_token')
+      const token_type = sessionStorage.getItem('token_type')
+      console.debug('Requesting user', { access_token, token_type })
+      const data = await fetch('https://api.github.com/user', { method: 'GET', headers: { 'Authorization': `${token_type} ${access_token}` } }).then((res) => res.json())
+      console.debug('User data', { data })
+      return data
+    }
+    if (!login) fetchUser.then(() => { ref.current.innerHTML = UserInfo(data); })
   })
 
   return `
