9d7d9aa0f20e9a3feaae0a14001cbdd01a6c0636
Arturs Jansons <a.jansons@gmail.com>
2024-08-06 15:05:29 +0300

    Updates

--- 1990/1990.html.js
+++ 1990/1990.html.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-"use strict";var M=Object.create;var u=Object.defineProperty;var F=Object.getOwnPropertyDescriptor;var N=Object.getOwnPropertyNames;var z=Object.getPrototypeOf,D=Object.prototype.hasOwnProperty;var A=(t,e,o,$)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of N(e))!D.call(t,r)&&r!==o&&u(t,r,{get:()=>e[r],enumerable:!($=F(e,r))||$.enumerable});return t};var H=(t,e,o)=>(o=t!=null?M(z(t)):{},A(e||!t||!t.__esModule?u(o,"default",{value:t,enumerable:!0}):o,t));var l=H(require("fs")),p=(t,e)=>l.writeFileSync(t,e,{encoding:"utf8"}),g=t=>l.readFileSync(t,{encoding:"utf8"});var w=new Date,d=`v3.1.0-${w.toJSON()}`,n=btoa(`${Number(w)}`).slice(10,18);var Z=g("./src/pages/1990/styles.css"),q=Object.entries({"script-src":[`'nonce-${n}'`,"'strict-dynamic'","https://www.google-analytics.com","https://ssl.google-analytics.com","https://static.hotjar.com","https://js.sentry-cdn.com","'unsafe-inline'"],"img-src":["'self'","https://www.google-analytics.com","https://avatars.githubusercontent.com/","https://www.googletagmanager.com/"],"connect-src":["'self'","https://www.google-analytics.com","https://region1.google-analytics.com","https://in.hotjar.com","https://static.hotjar.com","https://github.com/login/oauth/access_token","https://api.github.com/graphql","https://api.github.com/user","https://qilg4ch66b3vpgtevzccb5meum0ttfcl.lambda-url.eu-north-1.on.aws/","https://o171820.ingest.sentry.io/"],"style-src":[`'nonce-${n}'`,"https://static.hotjar.com"],"object-src":["'none'"],"base-uri":["'none'"],"frame-src":["https://vars.hotjar.com/"]}).reduce((t,[e,o])=>`${t};${e} ${o.join(" ")}`,"default-src 'self'"),c="Art&#363;rs Jansons",m="Full-stack Developer",f="10+ year exp.",B=`${c} | ${m} [${f}]`,b="Mobile / Web Developer &amp; JavaScript Consultant",I="HTML, CSS, JavaScript, TypeScript, React, React Native, PHP, MySQL, Bash, Makefile, Docker, GraphQL, Next, Nest",k=()=>`<p>
+"use strict";var M=Object.create;var u=Object.defineProperty;var D=Object.getOwnPropertyDescriptor;var N=Object.getOwnPropertyNames;var F=Object.getPrototypeOf,O=Object.prototype.hasOwnProperty;var _=(t,e,o,$)=>{if(e&&typeof e=="object"||typeof e=="function")for(let c of N(e))!O.call(t,c)&&c!==o&&u(t,c,{get:()=>e[c],enumerable:!($=D(e,c))||$.enumerable});return t};var z=(t,e,o)=>(o=t!=null?M(F(t)):{},_(e||!t||!t.__esModule?u(o,"default",{value:t,enumerable:!0}):o,t));var l=z(require("fs")),p=(t,e)=>l.writeFileSync(t,e,{encoding:"utf8"}),d=t=>l.readFileSync(t,{encoding:"utf8"});var k=new Date,g=`v3.1.0-${k.toJSON()}`,n=btoa(`${Number(k)}`).slice(10,18);var Y=d("./src/pages/1990/styles.css"),A=Object.entries({"script-src":[`'nonce-${n}'`,"'strict-dynamic'","https://www.google-analytics.com","https://ssl.google-analytics.com","https://static.hotjar.com","https://js.sentry-cdn.com","'unsafe-inline'"],"img-src":["'self'","https://www.google-analytics.com","https://avatars.githubusercontent.com/","https://www.googletagmanager.com/"],"connect-src":["'self'","https://www.google-analytics.com","https://region1.google-analytics.com","https://in.hotjar.com","https://static.hotjar.com","https://github.com/login/oauth/access_token","https://api.github.com/graphql","https://api.github.com/user","https://qilg4ch66b3vpgtevzccb5meum0ttfcl.lambda-url.eu-north-1.on.aws/","https://o171820.ingest.sentry.io/"],"style-src":[`'nonce-${n}'`,"https://static.hotjar.com"],"object-src":["'none'"],"base-uri":["'none'"],"frame-src":["https://vars.hotjar.com/"]}).reduce((t,[e,o])=>`${t};${e} ${o.join(" ")}`,"default-src 'self'"),r="Art&#363;rs Jansons",m="Full-stack Developer",f="10+ year exp.",H=`${r} | ${m} [${f}]`,b="Mobile / Web Developer &amp; JavaScript Consultant",q="HTML, CSS, JavaScript, TypeScript, React, React Native, PHP, MySQL, Bash, Makefile, Docker, GraphQL, Next, Nest",w=()=>`<p>
                         1990 |
-                        <!-- <a href="/2000/">2000</a> |
-                        <a href="/2010/">2010</a> |
-                        <a href="/2020/">2020</a> | -->
-                        <a href="/next/">BETA</a>
+                        
+                        <a href="/next/">BETA</a> |
+                        <a id="toggleDosStyle" href="#">DOS</a>
                     </p>`,y=()=>`<font color="orange">HTML</font>,
                                         <font color="blue">CSS</font>,
                                         <font color="#F0DB4F">JavaScript</font>,
@@ -15,29 +14,29 @@
                                         <font color="#008ddb">Docker</font>,
                                         <font color="#dd34a6">GraphQL</font>,
                                         <font>Next</font>,
-                                        <font color="#da214c">Nest</font>`,h=({src:t})=>`<pre><font size=1>${g(t)}</font></pre>`,a=t=>`<a ${/^http/.test(t.href)?' rel="noopener noreferrer"':""} href="${t.href}" title="${t.title}">${t.title}</a>`,i=[{image:{ascii:"./src/pages/1990/assets/website.ascii"},description:"Website development, including complex forms with custom field validation, popups and dialogs.",links:[{href:"https://wiam-front-test.vercel.app/",title:"Ex. 1"},{href:"https://mindmap-opal.vercel.app/",title:"Ex. 2"},{href:"https://codepen.io/iegik/full/ObZpqo",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/mobile.ascii"},description:"Mobile applications - developing, publishing or upgrading existing ones.",links:[{href:"https://github.com/iegik/inventarizacija/",title:"Ex. 1"},{href:"https://github.com/iegik/react_calc/",title:"Ex. 2"}],comment:"Unfortunately apps are not available in Google Play anymore"},{image:{ascii:"./src/pages/1990/assets/extensions.ascii"},description:"Browser extensions, custom scripts and other researches where I'm testing new approaches.",links:[{href:"https://github.com/iegik/clock-extension",title:"Ex. 1"},{href:"https://github.com/iegik/thunar-scripts",title:"Ex. 2"},{href:"https://hub.docker.com/repository/docker/iegik/docker-node/general",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/cms.ascii"},description:"CMS (Wordpress, Magento) configuration, plugin creation, RESTFul API on PHP, GraphQL and microservices",links:[{href:"https://github.com/WinLinMac/magento_themes",title:"Ex. 1"}]}],j=[{link:{href:"mailto:a.jansons@gmail.com",title:"a.jansons@gmail.com"}},{link:{href:"https://t.me/ajansons",title:"t.me/ajansons"}}],x=[{href:"https://linkedin.com/in/iegik",title:"LinkedIn"},{href:"https://github.com/iegik",title:"GitHub"},{href:"https://profile.codersrank.io/user/iegik",title:"CodersRank"},{href:"https://codepen.io/iegik/",title:"CodePen"},{href:"https://jsfiddle.net/user/iegik",title:"JSFiddle"},{href:"https://stackoverflow.com/users/771471/iegik",title:"StackOverflow"}],S="'SFMono-Regular', 'SF Mono', 'Ubuntu Mono', Consolas, 'DejaVu Sans Mono', Menlo, monospace",s=t=>`<center>${h({src:t.image.ascii})}</center>
+                                        <font color="#da214c">Nest</font>`,h=({src:t})=>`<pre><font size=1>${d(t)}</font></pre>`,a=t=>`<a ${/^http/.test(t.href)?' rel="noopener noreferrer"':""} href="${t.href}" title="${t.title}">${t.title}</a>`,i=[{image:{ascii:"./src/pages/1990/assets/website.ascii"},description:"Website development, including complex forms with custom field validation, popups and dialogs.",links:[{href:"https://wiam-front-test.vercel.app/",title:"Ex. 1"},{href:"https://mindmap-opal.vercel.app/",title:"Ex. 2"},{href:"https://codepen.io/iegik/full/ObZpqo",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/mobile.ascii"},description:"Mobile applications - developing, publishing or upgrading existing ones.",links:[{href:"https://github.com/iegik/inventarizacija/",title:"Ex. 1"},{href:"https://github.com/iegik/react_calc/",title:"Ex. 2"}],comment:"Unfortunately apps are not available in Google Play anymore"},{image:{ascii:"./src/pages/1990/assets/extensions.ascii"},description:"Browser extensions, custom scripts and other researches where I'm testing new approaches.",links:[{href:"https://github.com/iegik/clock-extension",title:"Ex. 1"},{href:"https://github.com/iegik/thunar-scripts",title:"Ex. 2"},{href:"https://hub.docker.com/repository/docker/iegik/docker-node/general",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/cms.ascii"},description:"CMS (Wordpress, Magento) configuration, plugin creation, RESTFul API on PHP, GraphQL and microservices",links:[{href:"https://github.com/WinLinMac/magento_themes",title:"Ex. 1"}]}],j=[{link:{href:"mailto:a.jansons@gmail.com",title:"a.jansons@gmail.com"}},{link:{href:"https://t.me/ajansons",title:"t.me/ajansons"}}],S=[{href:"https://linkedin.com/in/iegik",title:"LinkedIn"},{href:"https://github.com/iegik",title:"GitHub"},{href:"https://profile.codersrank.io/user/iegik",title:"CodersRank"},{href:"https://codepen.io/iegik/",title:"CodePen"},{href:"https://jsfiddle.net/user/iegik",title:"JSFiddle"},{href:"https://stackoverflow.com/users/771471/iegik",title:"StackOverflow"}],x="'SFMono-Regular', 'SF Mono', 'Ubuntu Mono', Consolas, 'DejaVu Sans Mono', Menlo, monospace",s=t=>`<center>${h({src:t.image.ascii})}</center>
                                             <p>${t.description}
                                             <p>
                                                 ${t.links.map((e,o)=>`${o?" | ":""}${a(e)}`).join("")}
-                                            ${t.comment?`<p><em><font color="gray">${t.comment}</font></em>`:""}`,C=()=>"<p>Time Zone: EEST</p>",E=()=>"<p>Work Time: 10:00 - 20:00</p>",L=()=>"<p>Current Location: Earth</p>",P=()=>`<form align="left" method="get" action="/1990/mailto/">
+                                            ${t.comment?`<p><em><font color="gray">${t.comment}</font></em>`:""}`,C=()=>"<p>Time Zone: EEST</p>",E=()=>"<p>Work Time: 10:00 - 20:00</p>",T=()=>"<p>Current Location: Earth</p>",L=()=>`<form align="left" method="get" action="/1990/mailto/">
                                             <fieldset>
                                                 <legend>Bonus Code</legend>
-                                                ${h({src:"./src/pages/1990/assets/slotmachine.ascii"})}
+                                                <code>${h({src:"./src/pages/1990/assets/slotmachine.ascii"})}</code>
                                             </fieldset>
                                         </form>
-                                    </font>`,Q=()=>`<font face="${S}" size="3">
+                                    </font>`,B=()=>`<font face="${x}" size="3">
         <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
             <tr align="center">
                 <td valign="top">
-                    ${k()}
+                    ${w()}
                     <hr/>
                     <table border="0" cellpadding="0" cellspacing="8" width="800px">
                         <tr>
                             <td>
                                 ${h({src:"./src/pages/1990/assets/photo.ascii"})}
                             <td width="80%">
-                                <p>${c}
-                                <h1><font color="#ffff58" size="5"><b>${m}</b></font><font size="2">[${f}]</font></h1>
+                                <p>${r}
+                                <h1><font size="5"><b>${m}</b></font><font size="2">[${f}]</font></h1>
                                 <p><em>${b}</em></p>
                                 <p>
                                     <b>Tech Stack:</b>
@@ -76,11 +75,11 @@
                                     <br/>
                                     ${C()}
                                     ${E()}
-                                    ${L()}
+                                    ${T()}
                                 </font>
                             <td align="right">
                                 <font size="2">
-                                    ${P()}
+                                    ${L()}
                                 </font>
                     </table>
             <tr>
@@ -89,25 +88,25 @@
                     <table border="0" cellpadding="8" cellspacing="0" width="100%">
                         <tr>
                             <td align="center">
-                                <p>${x.map((t,e)=>`${e?" | ":""}${a(t)}`).join("")}</p>
+                                <p>${S.map((t,e)=>`${e?" | ":""}${a(t)}`).join("")}</p>
                                 <br/>
                                 <marquee>
-                                    <font color="gray">\xA9 2024 ${c} | All rights reserved</font>
+                                    <font color="gray">\xA9 2024 ${r} | All rights reserved</font>
                     </table>
         </table>
-    </font>`,R=()=>`<font face="${S}" size="3">
+    </font>`,I=()=>`<font face="${x}" size="3">
         <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
             <tr align="center">
                 <td valign="top">
-                    ${k()}
+                    ${w()}
                     <hr/>
                     <table border="0" cellpadding="0" cellspacing="8">
                         <tr>
                             <td align="center">
                                 ${h({src:"./src/pages/1990/assets/photo.ascii"})}
-                                <p>${c}
+                                <p>${r}
                                 <h1>
-                                    <font color="#ffff58" size="5"><b>${m}</b></font>
+                                    <font size="5"><b>${m}</b></font>
                                     <br/>
                                     <font size="2">[${f}]</font></h1>
                                 <p><em>${b}</em></p>
@@ -152,13 +151,13 @@
                                     <br/>
                                     ${C()}
                                     ${E()}
-                                    ${L()}
+                                    ${T()}
                                 </font>
                         </tr>
                         <tr>
                             <td width="60%" align="right">
                                 <font size="2">
-                                    ${P()}
+                                    ${L()}
                                 </font>
                     </table>
             <tr>
@@ -167,23 +166,23 @@
                     <table border="0" cellpadding="8" cellspacing="0" width="100%">
                         <tr>
                             <td align="center">
-                                <p>${x.map((t,e)=>`${e?" | ":""}${a(t)}`).join("")}</p>
+                                <p>${S.map((t,e)=>`${e?" | ":""}${a(t)}`).join("")}</p>
                                 <marquee>
-                                    <font color="gray">\xA9 2024 ${c} | All rights reserved</font>
+                                    <font color="gray">\xA9 2024 ${r} | All rights reserved</font>
                     </table>
         </table>
-    </font>`,T=t=>`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
+    </font>`,P=t=>`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
     "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Security-Policy" content="${q}">
-    <title>${B}</title>
+    <meta http-equiv="Content-Security-Policy" content="${A}">
+    <title>${H}</title>
     <meta name="description" content="${b}" />
-    <meta name="keywords" content="${I}" />
+    <meta name="keywords" content="${q}" />
     <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="var(--color-window)" />
-    <link rel="stylesheet" nonce="${n}" href="/1990/styles.min.css" />
+    
     
 </head>
 <body>
@@ -196,6 +195,24 @@
       gtag('js', new Date());
 
       gtag('config', 'G-5ZY8Y6X2C4');
+    </script>
+    <!-- DOS Theme Code for https://iegik.github.io -->
+    <script nonce="${n}">
+        (function(m,s,D,O,S,_){
+            O.onclick=a;
+            function a(){
+                O.innerText=m;
+                O.onclick=r;
+                _=D.createElement('link');_.async=1;
+                _.rel='stylesheet';_.href=S;_.nonce='${n}';
+                D.head.appendChild(_);
+            };
+            function r(){
+                O.innerText=s;
+                O.onclick=a;
+                _.remove();
+            };
+        })('NO STYLE', 'DOS', document, toggleDosStyle,'/1990/styles.min.css');
     </script>
     <!-- Hotjar Tracking Code for https://iegik.github.io -->
     <script nonce="${n}">
@@ -213,14 +230,14 @@
     document.addEventListener('DOMContentLoaded', () => {
       typeof Sentry !== 'undefined' && Sentry.init({
         dsn: "https://179618f1f04d4d9dac08acc750d5736c@o171820.ingest.sentry.io/1250596",
-        release: "1250596@${d}",
+        release: "1250596@${g}",
         environment: "production",
       });
     });
     </script>
-`;p("public/1990/desktop/index.html",T(Q()));p("public/1990/mobile/index.html",T(R()));var U=`
+`;p("public/1990/desktop/index.html",P(B()));p("public/1990/mobile/index.html",P(I()));var Q=`
 CACHE MANIFEST
-# rev ${d}
+# rev ${g}
 
 CACHE:
 index.html
@@ -231,4 +248,4 @@ index.html
 
 NETWORK:
 *
-`;p("public/manifest.appcache",U);
+`;p("public/manifest.appcache",Q);
