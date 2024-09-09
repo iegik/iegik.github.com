492b721e90cc42fcc86f932bdceb3c826df2c212
Arturs Jansons <a.jansons@gmail.com>
2024-09-09 23:21:02 +0300

    Updates

--- 1990/1990.html.js
+++ 1990/1990.html.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-"use strict";var Q=Object.create;var F=Object.defineProperty;var Y=Object.getOwnPropertyDescriptor;var V=Object.getOwnPropertyNames;var Z=Object.getPrototypeOf,X=Object.prototype.hasOwnProperty;var K=(t,e,o,a)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of V(e))!X.call(t,r)&&r!==o&&F(t,r,{get:()=>e[r],enumerable:!(a=Y(e,r))||a.enumerable});return t};var tt=(t,e,o)=>(o=t!=null?Q(Z(t)):{},K(e||!t||!t.__esModule?F(o,"default",{value:t,enumerable:!0}):o,t));var y=tt(require("fs")),m=(t,e)=>y.writeFileSync(t,e,{encoding:"utf8"}),c=t=>y.readFileSync(t,{encoding:"utf8"});var C=new Date,x=`v3.1.0-${C.toJSON()}`,i=btoa(`${Number(C)}`).slice(10,18);var h=t=>`<a ${/^http/.test(t.href)?' rel="noopener noreferrer"':""} href="${t.href}" title="${t.title}">${t.title}</a>`;var w=t=>t.replace(/[&<"']/g,e=>{switch(e){case"&":return"&amp;";case"<":return"&lt;";case'"':return"&quot;";default:return"&#039;"}});var v=({src:t})=>`<pre><font size=1>${w(c(t))}</font></pre>`;var S=({src:t,title:e})=>`<fieldset bgcolor="green">
+"use strict";var Y=Object.create;var M=Object.defineProperty;var V=Object.getOwnPropertyDescriptor;var Q=Object.getOwnPropertyNames;var Z=Object.getPrototypeOf,K=Object.prototype.hasOwnProperty;var X=(t,e,o,a)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of Q(e))!K.call(t,r)&&r!==o&&M(t,r,{get:()=>e[r],enumerable:!(a=V(e,r))||a.enumerable});return t};var tt=(t,e,o)=>(o=t!=null?Y(Z(t)):{},X(e||!t||!t.__esModule?M(o,"default",{value:t,enumerable:!0}):o,t));var y=tt(require("fs")),m=(t,e)=>y.writeFileSync(t,e,{encoding:"utf8"}),c=t=>y.readFileSync(t,{encoding:"utf8"});var C=new Date,x=`v3.1.0-${C.toJSON()}`,i=btoa(`${Number(C)}`).slice(10,18);var h=t=>`<a ${/^http/.test(t.href)?' rel="noopener noreferrer"':""} href="${t.href}" title="${t.title}">${t.title}</a>`;var w=t=>t.replace(/[&<"']/g,e=>{switch(e){case"&":return"&amp;";case"<":return"&lt;";case'"':return"&quot;";default:return"&#039;"}});var v=({src:t})=>`<pre><font size=1>${w(c(t))}</font></pre>`;var S=({src:t,title:e})=>`<fieldset bgcolor="green">
                                                 <legend>${e}</legend>
                                                 <code width="120px">${w(c(t))}</code>
-                                            </fieldset>`;var s=({srcDoc:t,src:e,nonce:o,async:a,prefix:r,postfix:d,iife:$,crossorigin:u="",integrity:g})=>{let f=`${r||""}${t?c(t):""}${d||""}`;return`<script ${a?"async":""} crossorigin="${u}" ${g?`integrity="${g}"`:""} nonce="${o}" ${e?`src="${e}"`:""}>${$?`(${f})(${$}})`:f}</script>`};var E=({nonce:t,gtmId:e})=>`
+                                            </fieldset>`;var s=({srcDoc:t,src:e,nonce:o,async:a,prefix:r,postfix:d,iife:$,crossorigin:u="",integrity:g})=>{let f=`${r||""}${t?c(t):""}${d||""}`;return`<script ${a?"async":""} crossorigin="${u}" ${g?`integrity="${g}"`:""} nonce="${o}" ${e?`src="${e}"`:""}>${$?`(${f})(${$})`:f}</script>`};var L=({nonce:t,gtmId:e})=>`
     ${s({nonce:t,srcDoc:"./src/lib/gtm.js",iife:`window,document,'script','dataLayer','${e}','${t}'`})}
-`,L=({nonce:t,gtmId:e})=>`<noscript><iframe nonce="${t}" src="https://www.googletagmanager.com/ns.html?id=${e}"
+`,E=({nonce:t,gtmId:e})=>`<noscript><iframe nonce="${t}" src="https://www.googletagmanager.com/ns.html?id=${e}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`;var D=({nonce:t,gtmId:e})=>`
     ${s({async:!0,nonce:t,src:`https://www.googletagmanager.com/gtag/js?id=${e}`})}
     ${s({srcDoc:"./src/lib/gtag.js",nonce:t,postfix:`gtag('config', '${e}');
@@ -19,10 +19,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`})}
-`;var I=({nonce:t,integrity:e,projectId:o,...a})=>`
-    ${s({async:!0,nonce:t,src:`https://js.sentry-cdn.com/${o}.min.js`,integrity:e})}
+`;var q=({nonce:t,integrity:e,projectId:o,...a})=>`
+    ${s({async:!0,nonce:t,src:"https://browser.sentry-cdn.com/8.29.0/bundle.tracing.min.js",integrity:e})}
     ${s({srcDoc:"./src/lib/sentry.js",nonce:t,iife:JSON.stringify(a)})}
-`;var et=c("./src/pages/1990/styles.css"),ot=Object.entries({"script-src":[`'nonce-${i}'`,"'strict-dynamic'","https://www.google-analytics.com","https://ssl.google-analytics.com","https://static.hotjar.com","https://js.sentry-cdn.com"],"img-src":["'self'","https://www.google-analytics.com","https://avatars.githubusercontent.com/","https://www.googletagmanager.com/"],"connect-src":["'self'","https://www.google-analytics.com","https://region1.google-analytics.com","https://in.hotjar.com","https://static.hotjar.com","https://content.hotjar.io","https://github.com/login/oauth/access_token","https://api.github.com/graphql","https://api.github.com/user","https://qilg4ch66b3vpgtevzccb5meum0ttfcl.lambda-url.eu-north-1.on.aws/","https://o171820.ingest.sentry.io/"],"style-src":[`'nonce-${i}'`,"https://static.hotjar.com"],"object-src":["'none'"],"base-uri":["'none'"],"frame-src":["https://vars.hotjar.com/"],"script-src-elem":[`'nonce-${i}'`,"'self'"]}).reduce((t,[e,o])=>`${t};${e} ${o.join(" ")}`,"default-src 'self'"),b="Art&#363;rs Jansons",j="Full-stack Developer",P="10+ year exp.",it=`${b} | ${j} [${P}]`,T="Mobile / Web Developer &amp; JavaScript Consultant",nt="HTML, CSS, JavaScript, TypeScript, React, React Native, PHP, MySQL, Bash, Makefile, Docker, GraphQL, Next, Nest",q=()=>`<p>
+`;var et=c("./src/pages/1990/styles.css"),ot=Object.entries({"script-src":[`'nonce-${i}'`,"'strict-dynamic'","https://www.google-analytics.com","https://ssl.google-analytics.com","https://static.hotjar.com","https://js.sentry-cdn.com"],"img-src":["'self'","https://www.google-analytics.com","https://avatars.githubusercontent.com/","https://www.googletagmanager.com/"],"connect-src":["'self'","https://www.google-analytics.com","https://region1.google-analytics.com","https://in.hotjar.com","https://static.hotjar.com","https://content.hotjar.io","https://github.com/login/oauth/access_token","https://api.github.com/graphql","https://api.github.com/user","https://qilg4ch66b3vpgtevzccb5meum0ttfcl.lambda-url.eu-north-1.on.aws/","https://o171820.ingest.sentry.io/"],"style-src":[`'nonce-${i}'`,"https://static.hotjar.com"],"object-src":["'none'"],"base-uri":["'none'"],"frame-src":["https://vars.hotjar.com/"],"script-src-elem":[`'nonce-${i}'`,"'self'"]}).reduce((t,[e,o])=>`${t};${e} ${o.join(" ")}`,"default-src 'self'"),b="Art&#363;rs Jansons",j="Full-stack Developer",P="10+ year exp.",it=`${b} | ${j} [${P}]`,T="Mobile / Web Developer &amp; JavaScript Consultant",nt="HTML, CSS, JavaScript, TypeScript, React, React Native, PHP, MySQL, Bash, Makefile, Docker, GraphQL, Next, Nest",I=()=>`<p>
                         1990 |
                         
                         <a href="/next/">BETA</a> |
@@ -37,11 +37,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                         <font color="#008DDB">Docker</font>,
                                         <font color="#DD34A6">GraphQL</font>,
                                         <font>Next</font>,
-                                        <font color="#DA214C">Nest</font>`,l=[{image:{ascii:"./src/pages/1990/assets/website.ascii"},description:"Website development, including complex forms with custom field validation, popups and dialogs.",links:[{href:"https://wiam-front-test.vercel.app/",title:"Ex. 1"},{href:"https://mindmap-opal.vercel.app/",title:"Ex. 2"},{href:"https://codepen.io/iegik/full/ObZpqo",title:"Ex. 3"},{href:"https://test-kanvajs.vercel.app/",title:"Ex. 4"}]},{image:{ascii:"./src/pages/1990/assets/mobile.ascii"},description:"Mobile applications - developing, publishing or upgrading existing ones.",links:[{href:"https://github.com/iegik/inventarizacija/",title:"Ex. 1"},{href:"https://github.com/iegik/react_calc/",title:"Ex. 2"}],comment:"Unfortunately apps are not available in Google Play anymore"},{image:{ascii:"./src/pages/1990/assets/extensions.ascii"},description:"Browser extensions, custom scripts and other researches where I'm testing new approaches.",links:[{href:"https://github.com/iegik/clock-extension",title:"Ex. 1"},{href:"https://github.com/iegik/thunar-scripts",title:"Ex. 2"},{href:"https://hub.docker.com/repository/docker/iegik/docker-node/general",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/cms.ascii"},description:"CMS (Wordpress, Magento) configuration, plugin creation, RESTFul API on PHP, GraphQL and microservices",links:[{href:"https://github.com/WinLinMac/magento_themes",title:"Ex. 1"}]}],_=[{link:{href:"https://t.me/ajansons",title:"t.me/ajansons"}}],H=[{href:"https://linkedin.com/in/iegik",title:"LinkedIn"},{href:"https://github.com/iegik",title:"GitHub"},{href:"https://profile.codersrank.io/user/iegik",title:"CodersRank"},{href:"https://codepen.io/iegik/",title:"CodePen"},{href:"https://jsfiddle.net/user/iegik",title:"JSFiddle"},{href:"https://stackoverflow.com/users/771471/iegik",title:"StackOverflow"}],N="'SFMono-Regular', 'SF Mono', 'Ubuntu Mono', Consolas, 'DejaVu Sans Mono', Menlo, monospace",p=t=>`<center>${v({src:t.image.ascii})}</center>
+                                        <font color="#DA214C">Nest</font>`,l=[{image:{ascii:"./src/pages/1990/assets/website.ascii"},description:"Website development, including complex forms with custom field validation, popups and dialogs.",links:[{href:"https://wiam-front-test.vercel.app/",title:"Ex. 1"},{href:"https://mindmap-opal.vercel.app/",title:"Ex. 2"},{href:"https://codepen.io/iegik/full/ObZpqo",title:"Ex. 3"},{href:"https://test-kanvajs.vercel.app/",title:"Ex. 4"}]},{image:{ascii:"./src/pages/1990/assets/mobile.ascii"},description:"Mobile applications - developing, publishing or upgrading existing ones.",links:[{href:"https://github.com/iegik/inventarizacija/",title:"Ex. 1"},{href:"https://github.com/iegik/react_calc/",title:"Ex. 2"}],comment:"Unfortunately apps are not available in Google Play anymore"},{image:{ascii:"./src/pages/1990/assets/extensions.ascii"},description:"Browser extensions, custom scripts and other researches where I'm testing new approaches.",links:[{href:"https://github.com/iegik/clock-extension",title:"Ex. 1"},{href:"https://github.com/iegik/thunar-scripts",title:"Ex. 2"},{href:"https://hub.docker.com/repository/docker/iegik/docker-node/general",title:"Ex. 3"}]},{image:{ascii:"./src/pages/1990/assets/cms.ascii"},description:"CMS (Wordpress, Magento) configuration, plugin creation, RESTFul API on PHP, GraphQL and microservices",links:[{href:"https://github.com/WinLinMac/magento_themes",title:"Ex. 1"}]}],_=[{link:{href:"https://t.me/ajansons",title:"t.me/ajansons"}}],H=[{href:"https://linkedin.com/in/iegik",title:"LinkedIn"},{href:"https://github.com/iegik",title:"GitHub"},{href:"https://profile.codersrank.io/user/iegik",title:"CodersRank"},{href:"https://codepen.io/iegik/",title:"CodePen"},{href:"https://jsfiddle.net/user/iegik",title:"JSFiddle"},{href:"https://stackoverflow.com/users/771471/iegik",title:"StackOverflow"}],A="'SFMono-Regular', 'SF Mono', 'Ubuntu Mono', Consolas, 'DejaVu Sans Mono', Menlo, monospace",p=t=>`<center>${v({src:t.image.ascii})}</center>
                                             <p>${t.description}
                                             <p>
                                                 ${t.links.map((e,o)=>`${o?" | ":""}${h(e)}`).join("")}
-                                            ${t.comment?`<p><em><font color="gray">${t.comment}</font></em>`:""}`,B=()=>"<p>Time Zone: EEST</p>",A=()=>"<p>Work Time: 10:00 - 20:00</p>",O=()=>"<p>Current Location: Earth</p>",J={email:{kind:"vertical",required:!0},to:{type:"hidden",value:"a.jansons+web@gmail.com"},access_key:{type:"hidden",value:"c5540606-b7ca-4634-980a-13e2c50cd823"},redirect:{type:"hidden",value:"/1990/sent"},subject:{kind:"vertical",required:!0,data:["feedback","work","consultation","issue"]},message:{kind:"vertical",required:!0},send:{type:"submit"}},rt={email:"Your email",subject:"Subject",message:"Content",send:"Send message",feedback:"Feedback",work:"Work opportunity",consultation:"Consultation",issue:"Bug Report",botcheck:"I' m not a robot"},k=t=>rt[t]||t,st=()=>t=>e=>{let{name:o}=e,{kind:a,type:r}=J[o]||{};if(r==="hidden")return t(e);if(r==="submit")return`<input type="submit" name="${o}" value="${k(o)}" />`;let d=k(o);return a==="vertical"?`<label for="${o}">${d}</label>
+                                            ${t.comment?`<p><em><font color="gray">${t.comment}</font></em>`:""}`,B=()=>"<p>Time Zone: EEST</p>",N=()=>"<p>Work Time: 10:00 - 20:00</p>",O=()=>"<p>Current Location: Earth</p>",J={email:{kind:"vertical",required:!0},to:{type:"hidden",value:"a.jansons+web@gmail.com"},access_key:{type:"hidden",value:"c5540606-b7ca-4634-980a-13e2c50cd823"},redirect:{type:"hidden",value:"/1990/sent"},subject:{kind:"vertical",required:!0,data:["feedback","work","consultation","issue"]},message:{kind:"vertical",required:!0},send:{type:"submit"}},rt={email:"Your email",subject:"Subject",message:"Content",send:"Send message",feedback:"Feedback",work:"Work opportunity",consultation:"Consultation",issue:"Bug Report",botcheck:"I' m not a robot"},k=t=>rt[t]||t,st=()=>t=>e=>{let{name:o}=e,{kind:a,type:r}=J[o]||{};if(r==="hidden")return t(e);if(r==="submit")return`<input type="submit" name="${o}" value="${k(o)}" />`;let d=k(o);return a==="vertical"?`<label for="${o}">${d}</label>
                                                         <br>
                                                         <br>
                                                         ${t(e)}`:`<label for="${o}">${d}</label>&nbsp;${t(e)}`},at=({name:t,rows:e,cols:o})=>{let{data:a,type:r,value:d="",required:$}=J[t]||{},u=t,g=$?"required":"";return a?`<select id="${u}" name="${t}" ${g}>
@@ -95,11 +95,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                                             ${n({name:"send"})}
                                             </table>
                                         </fieldset>
-                                    </form>`,pt=()=>`<font face="${N}" size="3">
+                                    </form>`,pt=()=>`<font face="${A}" size="3">
         <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
             <tr align="center">
                 <td valign="top">
-                    ${q()}
+                    ${I()}
                     <hr>
                     <table border="0" cellpadding="0" cellspacing="8" width="768px">
                         <tr>
@@ -154,7 +154,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                     </ul>
                                     <br>
                                     ${B()}
-                                    ${A()}
+                                    ${N()}
                                     ${O()}
                                 </font>
                             <td valign="bottom" align="right">
@@ -180,11 +180,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                     <font color="gray">\xA9 2024 ${b} | All rights reserved</font>
                     </table>
         </table>
-    </font>`,dt=()=>`<font face="${N}" size="3">
+    </font>`,dt=()=>`<font face="${A}" size="3">
         <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
             <tr align="center">
                 <td valign="top">
-                    ${q()}
+                    ${I()}
                     <hr>
                     <table border="0" cellpadding="0" cellspacing="8">
                         <tr>
@@ -243,7 +243,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                     </ul>
                                     <br>
                                     ${B()}
-                                    ${A()}
+                                    ${N()}
                                     ${O()}
                                 </font>
                         <tr>
@@ -264,8 +264,8 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                     <font color="gray">\xA9 2024 ${b} | All rights reserved</font>
                     </table>
         </table>
-    </font>`,M=process.env.IS_VITE?"<!DOCTYPE html>":`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
-    "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">`,W=t=>`${M}
+    </font>`,F=process.env.IS_VITE?"<!DOCTYPE html>":`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
+    "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">`,W=t=>`${F}
 <html lang="en">
 <head>
     <meta http-equiv="Content-Security-Policy" content="${ot}">
@@ -279,7 +279,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     
     <style nonce="${i}" id="/1990/styles.min.css">${et}</style>
     
-    ${E({nonce:i,gtmId:"GTM-MBG56M"})}
+    ${L({nonce:i,gtmId:"GTM-MBG56M"})}
     
     ${z({nonce:i,hjid:2660383,hjsv:6})}
 </head>
@@ -287,13 +287,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     
     ${D({nonce:i,gtmId:"G-5ZY8Y6X2C4"})}
     
-    ${L({nonce:i,gtmId:"GTM-MBG56M"})}
+    ${E({nonce:i,gtmId:"GTM-MBG56M"})}
     ${t}
     
     ${s({srcDoc:"./src/lib/dosTheme.js",nonce:i,prefix:`const nonce = '${i}';
 `})}
-    ${I({nonce:i,projectId:"179618f1f04d4d9dac08acc750d5736c",dsn:"https://179618f1f04d4d9dac08acc750d5736c@o171820.ingest.sentry.io/1250596",release:`1250596@${x}`,environment:"production",integrity:"sha384-EmlJLN9Q0yu0/2UUCIYnEM88jpQ7xUhtNI2ZeXb/ci3cwoAoIQl350N4PQPlMbP5"})}
-`;m("public/index.html",`${M}
+    ${q({nonce:i,projectId:"179618f1f04d4d9dac08acc750d5736c",dsn:"https://179618f1f04d4d9dac08acc750d5736c@o171820.ingest.sentry.io/1250596",release:`1250596@${x}`,environment:"production",integrity:"sha384-6yzL+SsRi1vefLAU9+yqKb0YIeAiJ6GsCob5LxN8Af29Ze1Q5iCg0Ur2fwFroEqa"})}
+`;m("public/index.html",`${F}
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -317,7 +317,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                       </table>
                       <font size="1" color="lightgray">301 Redirect</font><br>
                       <font size="1" color="gray">You will be redirected to the new page shortly. If not, click <a id="url" href="1990">here</a>.</font>
-`);m("public/1990/index.html",`${M}
+`);m("public/1990/index.html",`${F}
 <html lang="en">
 <head>
     <meta charset="UTF-8">
