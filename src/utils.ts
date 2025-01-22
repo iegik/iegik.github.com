b9b731120fe82a0c3b1a1328a4c26308bab73768
Arturs Jansons <a.jansons@gmail.com>
2025-01-23 00:08:57 +0200

    bump

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ export * from './utils.node';
 // export * from './utils.deno';
 
 const timestamp = new Date();
-export const release = `v3.1.2-${timestamp.toJSON()}`
+export const release = `v3.1.4-${timestamp.toJSON()}`
 export const nonce = btoa(`${Number(timestamp)}`).slice(10,18)
