a78948a169fe96d78f6920e8ba3e4120e1acc9c1
Arturs Jansons <a.jansons@gmail.com>
2025-04-01 11:09:19 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'v3.1.5';
-const cacheAllowlist = ["v3.1.4", "v3.1.5"];
+const cacheName = 'v3.1.6';
+const cacheAllowlist = ["v3.1.4", "v3.1.6"];
 const contentToCache = [
   '/index.html',
   '/1990/index.html',
